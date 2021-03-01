e2cbd4ffc14cf3d334c5a7a6ba46c431badac20e
Gerardo Garza <owlscloud@gmail.com>
2021-02-28 18:34:19 -0600

    typography

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home";
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+  },
+});
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route component={Home} path="/" exact />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route component={Home} path="/" exact />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Typography } from "@material-ui/core";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Container>
+        <Typography variant="h3" aling="left">
+          Hello
+        </Typography>
+        <Typography variant="h5" aling="left">
+          I am selling clothes
+        </Typography>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
